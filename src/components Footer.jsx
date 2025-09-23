c1be80c4a63f8c5004283d8920c8bde9cd5061e8
JustinTafua <j.tafua1990@gmail.com>
2025-09-23 17:55:10 -0500

    Delete src/components Footer.jsx

--- src/components Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-  return (
-    <footer style={{padding:'24px',borderTop:'1px solid #eee',textAlign:'center',opacity:.8}}>
-      <small>© 2025 Justin Tafua • CarScan Pro</small>
-    </footer>
-  );
-}
